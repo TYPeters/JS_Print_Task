37a96fac39473c05bf5840fb372c15d996674b90
ty_Peterz <105292564+TYPeters@users.noreply.github.com>
2022-06-24 19:57:43 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+let myName="Peter Terungwa"
+let myHeight="5.7ft";
+let myCountry="Nigeria"
+document.write("My name:"+" "+myName+"<br>"+ " "+ "My height:"+" "+myHeight+"<br>"+ "My country:"+" "+myCountry)
